abde806029ca62889da6a91873ed5a981810939f
Vincent Longpre <vincent.longpre1729@gmail.com>
2023-03-07 23:11:56 -0500

    Changed visulization function to add comparison

--- climatenet/utils/utils.py
+++ climatenet/utils/utils.py
@@ -55,21 +55,31 @@ class Config():
     
 def results_plots(save_dir: str):
     sns.set()
-    with open(path.join(save_dir,'trainResults.json'),'r') as f:
-        result_dict = json.load(f)
-    train_losses = result_dict['train_losses']
-    train_ious = result_dict['train_ious']
+    with open(path.join(save_dir,'trainResultsRand.json'),'r') as f:
+        resultRand_dict = json.load(f)
+    train_losses_rand = resultRand_dict['train_losses']
+    train_ious_rand = resultRand_dict['train_ious']
+
+    with open(path.join(save_dir,'trainResultsCurr.json'),'r') as f:
+        resultRand_dict = json.load(f)
+    train_losses_curr = resultRand_dict['train_losses']
+    train_ious_curr = resultRand_dict['train_ious']
 
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
-    ax1.plot(train_losses)
+    l1, = ax1.plot(train_losses_rand, label='Radom Sampling')
+    l2, = ax1.plot(train_losses_curr, label='Curriculum Sampling')
     ax1.set_title('Training Loss')
     ax1.set_xlabel('Iterations')
     ax1.set_ylabel('Jaccard Loss')
 
-    ax2.plot(train_ious)
+    ax2.plot(train_ious_rand, label='Radom Sampling')
+    ax2.plot(train_ious_curr, label='Curriculum Sampling')
     ax2.set_title('Training Mean IOUs')
     ax2.set_xlabel('Epochs')
     ax2.set_ylabel('Mean IOU')
+
+    fig.legend([l1,l2],['Random Sampling', 'Curriculum Sampling'], ncol=2, loc='upper center', bbox_to_anchor=(0.5, -0.01),
+          fancybox=True, facecolor='white')
     plt.savefig(path.join(save_dir,'resultsFigures.png'), bbox_inches='tight')
 
 results_plots('/Users/vincentlongpre/Documents/Devoirs/IFT6759/ift6759/results/') 
