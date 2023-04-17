3765853130a38e9e559b15ca5691ea27b2a56e80
Felix Rouleau <felix.rouleau@gmail.com>
2023-04-17 14:22:22 -0400

    Added docstring

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ from climatenet.utils.data import ClimateDatasetLabeled
 from climatenet.utils.utils import Config
 
 
-def finetuning(train_data_dir: str, config: Config, save_dir: str = 'results', method: str = 'classifier') -> (float, ndarray):
+def finetuning(train_data_dir: str, config: Config, save_dir: str = 'results', method: str = 'classifier') -> (
+        float, ndarray):
+    """
+    Finetuning phase of the CGNet model
+    :param train_data_dir: The path to the directory containing the dataset (in form of .nc files)
+    :param config: The model configuration.
+    :param save_dir: The directory to save the results to
+    :param method: The method to use for the fine-tuning ('finetune', 'inception', 'classifier')
+    :return: Weighted jacard score and ndarray of iou
+    """
     config.pretraining = 1
     cgnet = CGNet(config)
     test = ClimateDatasetLabeled(path.join(train_data_path, 'testSet'), config)
@@ -22,6 +31,15 @@ def finetuning(train_data_dir: str, config: Config, save_dir: str = 'results', m
 
 
 def training(train_data_path: str, config: Config, curriculum: bool, save_dir: str = 'results') -> (float, ndarray):
+    """
+    Training phase of the CGNet model
+    :param train_data_path: The path to the directory containing the dataset (in form of .nc files)
+    :param config: The model configuration.
+    :param curriculum: Whether to use curriculum learning or not (True/False) Curriculum learning is not used by
+    default and needs the data to be in a specific
+    :param save_dir: The directory to save the results to
+    :return:
+    """
     config.pretraining = 0
     test = ClimateDatasetLabeled(path.join(train_data_path, 'testSet'), config)
     cgnet = CGNet(config, save_dir=save_dir)
