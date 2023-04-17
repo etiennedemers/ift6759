cd267fe3ef370cde20a2ae8faca8b7f8653b03f5
Felix Rouleau <felix.rouleau@gmail.com>
2023-04-17 14:45:42 -0400

    Automated downloading and creation of datasets with get_data.py

---
+++ get_data.py
@@ -0,0 +1,57 @@
+if __name__ == "__main__":
+    import requests
+    import os
+    from tqdm import tqdm
+
+    from climatenet.utils.data import create_datasets
+    from climatenet.utils.metrics import currScore
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists('models'):
+        os.mkdir('models')
+
+    if not os.path.exists('data/train'):
+        os.mkdir('data/train')
+    if not os.path.exists('data/test'):
+        os.mkdir('data/test')
+    if not os.path.exists('models/original_baseline_model'):
+        os.mkdir('models/original_baseline_model')
+    if not os.path.exists('models/new_baseline_model'):
+        os.mkdir('models/new_baseline_model')
+
+    files = []
+    url = 'https://portal.nersc.gov/project/ClimateNet/climatenet_new/'
+    r = requests.get(url + "train/", allow_redirects=True)
+    for line in tqdm(r.iter_lines(), desc="Getting train file names"):
+        if "data-" in str(line):
+            files.append((str(line).split('href="')[1]).split('">data')[0])
+
+    for file in tqdm(files, desc="Downloading train files"):
+        file_url = url + "train/" + file
+        r = requests.get(file_url, allow_redirects=True)
+        with open("data/train/" + file, 'wb') as f:
+            f.write(r.content)
+
+    files = []
+    r = requests.get(url + "test/", allow_redirects=True)
+    for line in tqdm(r.iter_lines(), desc="Getting test file names"):
+        if "data-" in str(line):
+            files.append((str(line).split('href="')[1]).split('">data')[0])
+
+    for file in tqdm(files, desc="Downloading test files"):
+        file_url = url + "test/" + file
+        r = requests.get(file_url, allow_redirects=True)
+        with open("data/test/" + file, 'wb') as f:
+            f.write(r.content)
+
+    with open("models/new_baseline_model/weights.pth", "wb") as f:
+        r = requests.get(url + "model/weights_new.pth", allow_redirects=True)
+        f.write(r.content)
+
+    with open("models/original_baseline_model/weights.pth", "wb") as f:
+        r = requests.get(url + "model/weights.pth", allow_redirects=True)
+        f.write(r.content)
+
+    currScore("data/currScore.json", scoring_method="mean")
+    create_datasets(data_path="data/", ood=False)
