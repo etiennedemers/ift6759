bc90adfc031ccef9033269d187be5ca966f4446a
Felix Rouleau <felix.rouleau@gmail.com>
2023-04-16 21:56:10 -0400

    Formatted files

--- climatenet/models.py
+++ climatenet/models.py
@@ -1,6 +1,6 @@
 ###########################################################################
-#CGNet: A Light-weight Context Guided Network for Semantic Segmentation
-#Paper-Link: https://arxiv.org/pdf/1811.08201.pdf
+# CGNet: A Light-weight Context Guided Network for Semantic Segmentation
+# Paper-Link: https://arxiv.org/pdf/1811.08201.pdf
 ###########################################################################
 import torch
 import torch.nn as nn
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 import xarray as xr
-from time import time 
+from time import time
 from climatenet.utils.utils import Config
 from os import path, makedirs
 import json
@@ -27,10 +27,11 @@ class Interpolate(nn.Module):
         self.interp = nn.functional.interpolate
         self.size = size
         self.mode = mode
-    
+
     def forward(self, x):
-        x = self.interp(x, size=self.size, mode=self.mode,align_corners = False)
+        x = self.interp(x, size=self.size, mode=self.mode, align_corners=False)
         return x
+
 
 class CGNet():
     '''
@@ -56,7 +57,8 @@ class CGNet():
         Stores the optimizer we use for training the model
     '''
 
-    def __init__(self, config: Config = None, model_path: str = None, save_dir: str = 'results/', transition_method='finetuning'):
+    def __init__(self, config: Config = None, model_path: str = None, save_dir: str = 'results/',
+                 transition_method='finetuning'):
         self.transition_method = transition_method
         self.config = config
 
@@ -65,38 +67,42 @@ class CGNet():
             self.config = config
             if config is None:
                 self.config = Config(path.join(model_path, 'config.json'))
-            self.network = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)), pretraining=self.config.pretraining, finetuning=self.config.pretraining).cuda()
+            self.network = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)),
+                                       pretraining=self.config.pretraining, finetuning=self.config.pretraining).cuda()
             self.network.load_state_dict(torch.load(path.join(model_path, 'weights.pth')))
             if self.config.pretraining:
                 self.model_transition()
 
         elif config is not None:
             # Create new model
-            self.network = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)), pretraining=self.config.pretraining).cuda()
-        
-        else:
-                raise ValueError('''You need to specify either a config or a model path.''')
+            self.network = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)),
+                                       pretraining=self.config.pretraining).cuda()
 
-        self.optimizer = Adam(self.network.parameters(), lr=self.config.lr) 
+        else:
+            raise ValueError('''You need to specify either a config or a model path.''')
+
+        self.optimizer = Adam(self.network.parameters(), lr=self.config.lr)
         self.save_dir = save_dir
         self.model_path = model_path
 
     def model_transition(self):
-        if self.transition_method not in ['finetune','inception','classifier']:
+        if self.transition_method not in ['finetune', 'inception', 'classifier']:
             raise Exception('Invalid transition method!')
-        
+
         if self.transition_method != 'finetune':
             for p in self.network.parameters():
                 p.requires_grad = False
 
         if self.transition_method == 'inception':
-            finalNet = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)), pretraining=0).cuda()
+            finalNet = CGNetModule(classes=len(self.config.labels), channels=len(list(self.config.fields)),
+                                   pretraining=0).cuda()
             finalNet.in_channels = 256
-            self.network.classifier = nn.Sequential(ConvBNPReLU(256, 4, 3, 2),Interpolate(size=(768,1152),mode='bilinear'),finalNet).cuda()
+            self.network.classifier = nn.Sequential(ConvBNPReLU(256, 4, 3, 2),
+                                                    Interpolate(size=(768, 1152), mode='bilinear'), finalNet).cuda()
 
         elif self.transition_method in ['classifier', 'finetune']:
-            self.network.classifier = nn.Sequential(ConvBNPReLU(256, 128, 3, 2), Conv(128,len(self.config.labels),1,1)).cuda()
-
+            self.network.classifier = nn.Sequential(ConvBNPReLU(256, 128, 3, 2),
+                                                    Conv(128, len(self.config.labels), 1, 1)).cuda()
 
     def pretrain(self):
         self.network.train()
@@ -104,21 +110,21 @@ class CGNet():
         start = time()
         train_dataset = ClimateDatasetLabeled('data/pretrainSet', self.config)
         collate = ClimateDatasetLabeled.collate
-        loader = DataLoader(train_dataset, batch_size=self.config.train_batch_size, collate_fn=collate, num_workers=1, shuffle=True)
+        loader = DataLoader(train_dataset, batch_size=self.config.train_batch_size, collate_fn=collate, num_workers=1,
+                            shuffle=True)
         for epoch in range(self.config.epochs['pretrain']):
 
-            print(f'Epoch {epoch+1}:')
+            print(f'Epoch {epoch + 1}:')
             epoch_loader = tqdm(loader)
 
             train_quad_losses = []
             train_batch_sizes = []
 
             for features, labels in epoch_loader:
-            
                 # Push data on GPU and pass forward
                 features = torch.tensor(features.values).cuda()
                 labels = features.clone()
-                    
+
                 outputs = self.network(features)
 
                 # Pass backward
@@ -139,8 +145,8 @@ class CGNet():
             print(f'Epoch Loss:{weighted_average_quad_losses}')
 
         end = time()
-        hours = (end-start) // 3600
-        minutes = ((end-start) - (hours * 3600)) / 60
+        hours = (end - start) // 3600
+        minutes = ((end - start) - (hours * 3600)) / 60
         print(f'End of training. Total training time of {hours} hours and {minutes} minutes')
         print(f'Writing training logs to {self.save_dir}...')
         makedirs(self.save_dir, exist_ok=True)
@@ -152,8 +158,7 @@ class CGNet():
                 indent=4,
             ))
 
-        
-    def train(self,train_data_path,curriculum: bool = False):
+    def train(self, train_data_path, curriculum: bool = False):
         '''Train the network on the given dataset for the given amount of epochs'''
         self.network.train()
         train_losses = []
@@ -162,24 +167,25 @@ class CGNet():
         val_ious = []
 
         if curriculum:
-            training_phases = ['simple','medium','hard']
+            training_phases = ['simple', 'medium', 'hard']
         else:
             training_phases = ['union']
 
-        val_dataset = ClimateDatasetLabeled(train_data_path+'/validationSet', self.config)
+        val_dataset = ClimateDatasetLabeled(train_data_path + '/validationSet', self.config)
 
         start = time()
         for phase in training_phases:
             print(f'Starting {phase} training phase...')
-            train_dataset = ClimateDatasetLabeled(train_data_path+phase+'Set', self.config)
+            train_dataset = ClimateDatasetLabeled(train_data_path + phase + 'Set', self.config)
             collate = ClimateDatasetLabeled.collate
-            loader = DataLoader(train_dataset, batch_size=self.config.train_batch_size, collate_fn=collate, num_workers=1, shuffle=True)
+            loader = DataLoader(train_dataset, batch_size=self.config.train_batch_size, collate_fn=collate,
+                                num_workers=1, shuffle=True)
             for epoch in range(self.config.epochs[phase]):
 
-                print(f'Epoch {epoch+1}:')
+                print(f'Epoch {epoch + 1}:')
                 epoch_loader = tqdm(loader)
-                aggregate_cm = np.zeros((3,3))
-                
+                aggregate_cm = np.zeros((3, 3))
+
                 train_jaccard_losses = []
                 train_batch_sizes = []
 
@@ -187,11 +193,10 @@ class CGNet():
                 train_batch_sizes = []
 
                 for features, labels in epoch_loader:
-            
                     # Push data on GPU and pass forward
                     features = torch.tensor(features.values).cuda()
                     labels = torch.tensor(labels.values).cuda()
-                    
+
                     outputs = torch.softmax(self.network(features), 1)
 
                     # Update training CM
@@ -204,7 +209,7 @@ class CGNet():
                     loss.backward()
                     self.optimizer.step()
                     self.optimizer.zero_grad()
-                    
+
                     train_jaccard_losses.append(loss.item())
                     train_batch_sizes.append(labels.shape[0])
 
@@ -218,15 +223,16 @@ class CGNet():
                 val_ious.append(val_iou.mean())
                 train_batch_sizes = np.array(train_batch_sizes)
                 train_jaccard_losses = np.array(train_jaccard_losses)
-                weighted_average_jaccard_losses = (train_batch_sizes * train_jaccard_losses).sum() / train_batch_sizes.sum()
+                weighted_average_jaccard_losses = (
+                                                              train_batch_sizes * train_jaccard_losses).sum() / train_batch_sizes.sum()
                 train_losses.append(weighted_average_jaccard_losses)
                 train_ious.append(ious.mean())
                 print('IOUs: ', ious, ', mean: ', ious.mean())
                 print('Val IOUs:', val_iou, ', mean:', val_iou.mean())
 
         end = time()
-        hours = (end-start) // 3600
-        minutes = ((end-start) - (hours * 3600)) / 60
+        hours = (end - start) // 3600
+        minutes = ((end - start) - (hours * 3600)) / 60
         print(f'End of training. Total training time of {hours} hours and {minutes} minutes')
         print(f'Writing training logs to {self.save_dir}...')
         makedirs(self.save_dir, exist_ok=True)
@@ -251,16 +257,16 @@ class CGNet():
         predictions = []
         for batch in epoch_loader:
             features = torch.tensor(batch.values).cuda()
-        
+
             with torch.no_grad():
                 outputs = torch.softmax(self.network(features), 1)
             preds = torch.max(outputs, 1)[1].cpu().numpy()
 
             coords = batch.coords
             del coords['variable']
-            
+
             dims = [dim for dim in batch.dims if dim != "variable"]
-            
+
             predictions.append(xr.DataArray(preds, coords=coords, dims=dims, attrs=batch.attrs))
 
         return xr.concat(predictions, dim='time')
@@ -272,15 +278,14 @@ class CGNet():
         loader = DataLoader(dataset, batch_size=self.config.pred_batch_size, collate_fn=collate, num_workers=1)
 
         epoch_loader = tqdm(loader)
-        aggregate_cm = np.zeros((3,3))
+        aggregate_cm = np.zeros((3, 3))
         jaccard_losses = []
         batch_sizes = []
 
         for features, labels in epoch_loader:
-        
             features = torch.tensor(features.values).cuda()
             labels = torch.tensor(labels.values).cuda()
-                
+
             with torch.no_grad():
                 outputs = torch.softmax(self.network(features), 1)
             predictions = torch.max(outputs, 1)[1]
@@ -293,17 +298,17 @@ class CGNet():
         batch_sizes = np.array(batch_sizes)
         jaccard_losses = np.array(jaccard_losses)
         weighted_average_jaccard_losses = (batch_sizes * jaccard_losses).sum() / batch_sizes.sum()
-        
+
         if verbose:
             print('Evaluation stats:')
             print(aggregate_cm)
             print('IOUs: ', ious, ', mean: ', ious.mean())
 
         return weighted_average_jaccard_losses, ious
-    
+
     def get_cm_for_each_file(self, dataset: ClimateDatasetLabeled):
 
-        assert self.config.pred_batch_size==1, "can only get the ious for each file if pred_batch_size=1"
+        assert self.config.pred_batch_size == 1, "can only get the ious for each file if pred_batch_size=1"
 
         self.network.eval()
         collate = ClimateDatasetLabeled.collate
@@ -313,7 +318,6 @@ class CGNet():
         cms = []
 
         for features, labels in epoch_loader:
-
             features = torch.tensor(features.values).cuda()
             labels = torch.tensor(labels.values).cuda()
 
@@ -324,14 +328,13 @@ class CGNet():
             cms.append(get_cm(predictions, labels, 3))
 
         return np.stack(cms)
-        
 
     def save_model(self, save_path: str):
         '''
         Save model weights and config to a directory.
         '''
         # create save_path if it doesn't exist
-        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True) 
+        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
 
         # save weights and config
         self.config.save(path.join(save_path, 'config.json'))
@@ -352,7 +355,8 @@ class CGNetModule(nn.Module):
     CGNet (Wu et al, 2018: https://arxiv.org/pdf/1811.08201.pdf) implementation.
     This is taken from their implementation, we do not claim credit for this.
     """
-    def __init__(self, classes=19, channels=4, M=3, N= 21, dropout_flag = False, pretraining=False, finetuning=False):
+
+    def __init__(self, classes=19, channels=4, M=3, N=21, dropout_flag=False, pretraining=False, finetuning=False):
         """
         args:
           classes: number of classes in the dataset. Default is 19 for the cityscapes
@@ -364,45 +368,45 @@ class CGNetModule(nn.Module):
         self.finetuning = finetuning
         self.in_channels = channels
 
-        self.level1_0 = ConvBNPReLU(self.in_channels, 32, 3, 2)      # feature map size divided 2, 1/2
-        self.level1_1 = ConvBNPReLU(32, 32, 3, 1)                          
-        self.level1_2 = ConvBNPReLU(32, 32, 3, 1)      
+        self.level1_0 = ConvBNPReLU(self.in_channels, 32, 3, 2)  # feature map size divided 2, 1/2
+        self.level1_1 = ConvBNPReLU(32, 32, 3, 1)
+        self.level1_2 = ConvBNPReLU(32, 32, 3, 1)
 
-        self.sample1 = InputInjection(1)  #down-sample for Input Injection, factor=2
-        self.sample2 = InputInjection(2)  #down-sample for Input Injiection, factor=4
+        self.sample1 = InputInjection(1)  # down-sample for Input Injection, factor=2
+        self.sample2 = InputInjection(2)  # down-sample for Input Injiection, factor=4
 
         self.b1 = BNPReLU(32 + self.in_channels)
-        
-        #stage 2
-        self.level2_0 = ContextGuidedBlock_Down(32 + self.in_channels, 64, dilation_rate=2,reduction=8)  
+
+        # stage 2
+        self.level2_0 = ContextGuidedBlock_Down(32 + self.in_channels, 64, dilation_rate=2, reduction=8)
         self.level2 = nn.ModuleList()
-        for i in range(0, M-1):
-            self.level2.append(ContextGuidedBlock(64 , 64, dilation_rate=2, reduction=8))  #CG block
+        for i in range(0, M - 1):
+            self.level2.append(ContextGuidedBlock(64, 64, dilation_rate=2, reduction=8))  # CG block
         self.bn_prelu_2 = BNPReLU(128 + self.in_channels)
-        
-        #stage 3
-        self.level3_0 = ContextGuidedBlock_Down(128 + self.in_channels, 128, dilation_rate=4, reduction=16) 
+
+        # stage 3
+        self.level3_0 = ContextGuidedBlock_Down(128 + self.in_channels, 128, dilation_rate=4, reduction=16)
         self.level3 = nn.ModuleList()
-        for i in range(0, N-1):
-            self.level3.append(ContextGuidedBlock(128 , 128, dilation_rate=4, reduction=16)) # CG block
+        for i in range(0, N - 1):
+            self.level3.append(ContextGuidedBlock(128, 128, dilation_rate=4, reduction=16))  # CG block
         self.bn_prelu_3 = BNPReLU(256)
 
         if dropout_flag and not pretraining:
             print("have droput layer")
-            self.classifier = nn.Sequential(nn.Dropout2d(0.1, False),Conv(256, classes, 1, 1))
+            self.classifier = nn.Sequential(nn.Dropout2d(0.1, False), Conv(256, classes, 1, 1))
         elif not dropout_flag and not pretraining:
             self.classifier = nn.Sequential(Conv(256, classes, 1, 1))
         else:
             self.classifier = nn.Sequential(Conv(256, channels, 1, 1))
 
-        #init weights
+        # init weights
         for m in self.modules():
             classname = m.__class__.__name__
-            if classname.find('Conv2d')!= -1:
+            if classname.find('Conv2d') != -1:
                 nn.init.kaiming_normal_(m.weight)
                 if m.bias is not None:
                     m.bias.data.zero_()
-                elif classname.find('ConvTranspose2d')!= -1:
+                elif classname.find('ConvTranspose2d') != -1:
                     nn.init.kaiming_normal_(m.weight)
                     if m.bias is not None:
                         m.bias.data.zero_()
@@ -426,34 +430,33 @@ class CGNetModule(nn.Module):
         output0 = self.level1_2(output0)
         inp1 = self.sample1(input)
         inp2 = self.sample2(input)
-        
+
         # stage 2
         output0_cat = self.b1(torch.cat([output0, inp1], 1))
-        output1_0 = self.level2_0(output0_cat) # down-sampled
-        
+        output1_0 = self.level2_0(output0_cat)  # down-sampled
+
         for i, layer in enumerate(self.level2):
-            if i==0:
+            if i == 0:
                 output1 = layer(output1_0)
             else:
                 output1 = layer(output1)
 
-        output1_cat = self.bn_prelu_2(torch.cat([output1,  output1_0, inp2], 1))
+        output1_cat = self.bn_prelu_2(torch.cat([output1, output1_0, inp2], 1))
 
         # stage 3
-        output2_0 = self.level3_0(output1_cat) # down-sampled
+        output2_0 = self.level3_0(output1_cat)  # down-sampled
         for i, layer in enumerate(self.level3):
-            if i==0:
+            if i == 0:
                 output2 = layer(output2_0)
             else:
                 output2 = layer(output2)
 
         output2_cat = self.bn_prelu_3(torch.cat([output2_0, output2], 1))
-       
+
         # classifier
         classifier = self.classifier(output2_cat)
 
         # upsample segmenation map ---> the input image size
-        out = F.interpolate(classifier, input.size()[2:], mode='bilinear',align_corners = False)   #Upsample score map, factor=8
+        out = F.interpolate(classifier, input.size()[2:], mode='bilinear',
+                            align_corners=False)  # Upsample score map, factor=8
         return out
-      
-   
--- climatenet/utils/data.py
+++ climatenet/utils/data.py
@@ -7,6 +7,7 @@ import xarray as xr
 import numpy as np
 from climatenet.utils.utils import Config
 
+
 class ClimateDataset(Dataset):
     '''
     The basic Climate Dataset class. 
@@ -30,19 +31,19 @@ class ClimateDataset(Dataset):
     length : int
         Stores the amount of nc files in the Dataset
     '''
-  
+
     def __init__(self, path: str, config: Config):
         self.path: str = path
         self.fields: dict = config.fields
-        
+
         self.files: [str] = [f for f in sorted(listdir(self.path)) if f[-3:] == ".nc"]
         self.length: int = len(self.files)
-      
+
     def __len__(self):
         return self.length
 
     def normalize(self, features: xr.DataArray):
-        for variable_name, stats in self.fields.items():   
+        for variable_name, stats in self.fields.items():
             var = features.sel(variable=variable_name).values
             var -= stats['mean']
             var /= stats['std']
@@ -53,13 +54,14 @@ class ClimateDataset(Dataset):
         return features.transpose('time', 'variable', 'lat', 'lon')
 
     def __getitem__(self, idx: int):
-        file_path: str = path.join(self.path, self.files[idx]) 
+        file_path: str = path.join(self.path, self.files[idx])
         dataset = xr.load_dataset(file_path)
         return self.get_features(dataset)
 
     @staticmethod
     def collate(batch):
         return xr.concat(batch, dim='time')
+
 
 class ClimateDatasetLabeled(ClimateDataset):
     '''
@@ -68,33 +70,33 @@ class ClimateDatasetLabeled(ClimateDataset):
     '''
 
     def __getitem__(self, idx: int):
-        file_path: str = path.join(self.path, self.files[idx]) 
+        file_path: str = path.join(self.path, self.files[idx])
         dataset = xr.load_dataset(file_path)
         return self.get_features(dataset), dataset['LABELS']
 
-    @staticmethod 
+    @staticmethod
     def collate(batch):
         data, labels = map(list, zip(*batch))
         return xr.concat(data, dim='time'), xr.concat(labels, dim='time')
-    
-    
-def save_datefiles(files,output_path):
+
+
+def save_datefiles(files, output_path):
     for file in files:
         if file in listdir('data/test'):
             base_path = 'data/test/'
         else:
-             base_path = 'data/train/'
+            base_path = 'data/train/'
         makedirs(output_path, exist_ok=True)
         copy(path.join(base_path, file), output_path)
 
-    
-def sample_subset(output_path,n,method='rand'):
+
+def sample_subset(output_path, n, method='rand'):
     files = listdir('data/train')
     files.extend(listdir('data/test'))
     if method == 'rand':
         select_files = np.random.permutation(files)[:n]
     if method == 'curriculum':
-        with open('data/currScore.json','r') as f:
+        with open('data/currScore.json', 'r') as f:
             score_dict = json.load(f)
         select_dates = [k for k, v in sorted(score_dict.items(), key=lambda item: item[1], reverse=True)][:n]
         select_files = []
@@ -102,12 +104,12 @@ def sample_subset(output_path,n,method='rand'):
             reg = re.compile('data-' + date)
             select_files.append([file for file in files if reg.match(file)][0])
 
-    save_datefiles(select_files,output_path)
-    
+    save_datefiles(select_files, output_path)
 
-def create_datasets(data_path,ood=False):
-    with open('data/currScore.json','r') as f:
-            score_dict = json.load(f)
+
+def create_datasets(data_path, ood=False):
+    with open('data/currScore.json', 'r') as f:
+        score_dict = json.load(f)
 
     files = listdir('data/train')
     test_files = listdir('data/test')
@@ -119,11 +121,11 @@ def create_datasets(data_path,ood=False):
     for k in test_dates:
         score_dict.pop(k, None)
 
-    unique = [k for k, v in score_dict.items() if score_dict[k]==-1]
+    unique = [k for k, v in score_dict.items() if score_dict[k] == -1]
     val_dates = unique[:40]
     if not ood: test_dates = unique[40:][:61]
 
-    tresh_dict = {'test':'','validation':'','simple':0.55,'medium':0.475,'hard':-10}
+    tresh_dict = {'test': '', 'validation': '', 'simple': 0.55, 'medium': 0.475, 'hard': -10}
 
     for dataset, tresh in tresh_dict.items():
         print(dataset)
@@ -133,8 +135,8 @@ def create_datasets(data_path,ood=False):
             select_dates = test_dates
             print(test_dates)
         else:
-            select_dates = [k for k, v in score_dict.items() if score_dict[k]>tresh]
-        
+            select_dates = [k for k, v in score_dict.items() if score_dict[k] > tresh]
+
         if dataset != 'test':
             for k in select_dates:
                 score_dict.pop(k, None)
@@ -144,9 +146,8 @@ def create_datasets(data_path,ood=False):
             reg = re.compile('data-' + date)
             select_files.extend([file for file in files if reg.match(file)])
 
-        output_path = path.join(data_path, dataset+'Set')
-        save_datefiles(select_files,output_path)
+        output_path = path.join(data_path, dataset + 'Set')
+        save_datefiles(select_files, output_path)
         if dataset in ['simple', 'medium', 'hard']:
             output_path = path.join(data_path, 'unionSet')
-            save_datefiles(select_files,output_path)
-        
+            save_datefiles(select_files, output_path)
