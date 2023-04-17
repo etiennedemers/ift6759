5119d076f8499cf15e6c450a37d1b8cd70d7e8da
Felix Rouleau <felix.rouleau@gmail.com>
2023-04-16 21:11:33 -0400

    Added type hinting for main function and formatted it

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from climatenet.models import CGNet
-from climatenet.utils.utils import Config
-from climatenet.utils.data import ClimateDatasetLabeled
 from os import path
 
-def finetuning(train_data_dir,config,save_dir: str = 'results',method: str = 'classifier'):
+from numpy import ndarray
+
+from climatenet.models import CGNet
+from climatenet.utils.data import ClimateDatasetLabeled
+from climatenet.utils.utils import Config
+
+
+def finetuning(train_data_dir: str, config: Config, save_dir: str = 'results', method: str = 'classifier'):
     config.pretraining = 1
     cgnet = CGNet(config)
     test = ClimateDatasetLabeled(path.join(train_data_path, 'testSet'), config)
@@ -12,20 +16,21 @@ def finetuning(train_data_dir,config,save_dir: str = 'results',method: str = 'cl
     print('Saving Model...')
     cgnet.save_model('pretrained_cgnet')
     print('Starting Transition Phase')
-    cgnet = CGNet(config,model_path='pretrained_cgnet',save_dir=save_dir,transition_method=method)
+    cgnet = CGNet(config, model_path='pretrained_cgnet', save_dir=save_dir, transition_method=method)
     cgnet.train(train_data_dir)
-    cgnet.evaluate(test,verbose=True)
+    cgnet.evaluate(test, verbose=True)
 
-def training(train_data_path,config,curriculum,save_dir: str = 'results'):
+
+def training(train_data_path: str, config: Config, curriculum: bool, save_dir: str = 'results') -> (float, ndarray):
     config.pretraining = 0
     test = ClimateDatasetLabeled(path.join(train_data_path, 'testSet'), config)
-    cgnet = CGNet(config,save_dir=save_dir)
-    cgnet.train(train_data_path,curriculum=curriculum)
-    cgnet.evaluate(test,verbose=True)
+    cgnet = CGNet(config, save_dir=save_dir)
+    cgnet.train(train_data_path, curriculum=curriculum)
+    return cgnet.evaluate(test, verbose=True)
 
 
 if __name__ == "__main__":
     config = Config('models_configs/CGNet.json')
     train_data_path = 'data/ood/'
-    finetuning(train_data_path,config,method='finetune')
-    #training(train_data_path,config,False)
+    finetuning(train_data_path, config, method='finetune')
+    # training(train_data_path,config,False)
